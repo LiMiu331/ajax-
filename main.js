d97c8b59fef5713c7ab9cc46cf0605ba681d9045
libb <boomlibb1996@gmail.com>
2020-02-27 17:38:43 +0800

    自定义request 和 response

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
 mybutton.addEventListener('click', (e) => {
     let request = new XMLHttpRequest
-    request.open('GET', '/xxx') //配置request
-    request.send()
+    request.open('post', '/xxx') //配置request
+    request.setRequestHeader('bibi','18')
+    request.send('我偏要设置request body')
     request.onreadystatechange = () => {
         if (request.readyState === 4)
             console.log('请求响应都完毕了')
-        console.log(request.status)
-        if (request.readyState >= 200 && request.readyState < 300) {
-            console.log('说明请求成功')
-            console.log(typeof request.responseText)
-            console.log(request.responseText)
-            let string = request.responseText
-            //把符合JSON语法的字符串
-            //转换成JS对应的值
-            let object = JSON.parse(string)
-            //JSON.pase 是浏览器提供的
-            console.log(typeof object)
-            console.log(object)
-            console.log('object.note')
-            console.log(object.note)
-        } else if (request.readyState >= 400) {
+            console.log(request.status)
+            console.log(request.statusText)  //ok
+            if (request.status >= 200 && request.status < 300) {
+                console.log('说明请求成功')
+                console.log(request.getAllResponseHeaders(''))
+                console.log(request.getResponseHeader('Content-Type'))
+                console.log(request.responseText)
+                let string = request.responseText
+                //把符合JSON语法的字符串
+                //转换成JS对应的值
+                let object = JSON.parse(string)
+                //JSON.pase 是浏览器提供的
+                console.log(typeof object)
+                console.log(object)
+                console.log('object.note')
+                console.log(object.note)
+
+            } else if (request.readyState >= 400) {
             console.log('说明请求失败')
         }
     }
